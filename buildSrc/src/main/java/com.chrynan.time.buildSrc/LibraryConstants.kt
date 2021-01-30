83bef51cd5e17b49f101e920e6c143e4be2d6f65
Chris <ckeenan@starry.com>
2021-01-30 09:37:40 +0700

    Added buildSrc Module

---
+++ buildSrc/src/main/java/com.chrynan.time.buildSrc/LibraryConstants.kt
@@ -0,0 +1,17 @@
+package com.chrynan.time.buildSrc
+
+object LibraryConstants {
+
+    const val bintrayRepo = "chrynan"
+    const val group = "com.chrynan.time"
+    const val owner = "chrynan"
+    const val repoName = "time"
+    const val versionName = "0.1.0"
+    const val versionCode = 1
+    const val versionDescription = "First Major Release 0.1.0"
+    const val license = "Apache-2.0"
+    const val vcsUrl = "https://github.com/chRyNaN/time.git"
+    const val androidCompileSdkVersion = 29
+    const val androidMinSdkVersion = 23
+    const val androidTargetSdkVersion = 29
+}
