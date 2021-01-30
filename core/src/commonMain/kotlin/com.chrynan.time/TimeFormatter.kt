2de265b58c5b51a58504f19d0c90da06552d3c9b
Chris <ckeenan@starry.com>
2021-01-30 10:51:46 +0700

    Updated TimeFormatter

--- core/src/commonMain/kotlin/com.chrynan.time/TimeFormatter.kt
+++ core/src/commonMain/kotlin/com.chrynan.time/TimeFormatter.kt
@@ -5,18 +5,19 @@ package com.chrynan.time
 import kotlin.time.Duration
 import kotlin.time.ExperimentalTime
 
+/**
+ * A Utility interface to format [DateTimeString]s to UI text.
+ */
+@ExperimentalTime
 interface TimeFormatter {
 
     fun formatPastRelativeToNow(pastTime: DateTimeString): CharSequence
 
-    @ExperimentalTime
     fun formatPastDurationRelativeToNow(pastDuration: Duration): CharSequence
 
     fun formatFutureRelativeToNow(futureTime: DateTimeString): CharSequence
 
-    @ExperimentalTime
     fun formatFutureDurationRelativeToNow(futureDuration: Duration): CharSequence
 
-    @ExperimentalTime
     fun formatElapsedTime(duration: Duration): CharSequence
 }
