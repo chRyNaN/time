f49518d125e3513ccc9625e1a2756c04cd6e2500
Chris <ckeenan@starry.com>
2021-02-01 11:26:02 +0700

    Created DateTimeLongAsStringJsonSerializer

---
+++ time-core/src/commonMain/kotlin/com.chrynan.time/DateTimeLongAsStringJsonSerializer.kt
@@ -0,0 +1,44 @@
+@file:Suppress("unused")
+
+package com.chrynan.time
+
+import kotlinx.serialization.json.*
+
+object DateTimeLongAsStringJsonSerializer : JsonTransformingSerializer<DateTimeString>(DateTimeStringSerializer) {
+
+    override fun transformDeserialize(element: JsonElement): JsonElement {
+        if (element is JsonNull) return element
+
+        val primitive = element as JsonPrimitive
+
+        val stringValue = primitive.contentOrNull
+        val longValue = stringValue?.toLong()
+
+        return when {
+            longValue != null -> {
+                val value = longValue.toDateTimeLongFromMillisecondsSinceEpoch().toDateTimeString().value
+                JsonPrimitive(value)
+            }
+            stringValue != null -> JsonPrimitive(stringValue)
+            else -> element
+        }
+    }
+
+    override fun transformSerialize(element: JsonElement): JsonElement {
+        if (element is JsonNull) return element
+
+        val primitive = element as JsonPrimitive
+
+        val stringValue = primitive.contentOrNull
+        val longValue = stringValue?.toLong()
+
+        return when {
+            longValue != null -> {
+                val value = longValue.toDateTimeLongFromMillisecondsSinceEpoch().toDateTimeString().value
+                JsonPrimitive(value)
+            }
+            stringValue != null -> JsonPrimitive(stringValue)
+            else -> element
+        }
+    }
+}
