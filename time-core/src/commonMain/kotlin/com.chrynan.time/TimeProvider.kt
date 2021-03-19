27fe3ec11da82c886bbcb7a3830873f8f56704f0
Chris <ckeenan@starry.com>
2021-03-19 19:11:15 +0700

    Updated KDoc documentation in TimeProvider

--- time-core/src/commonMain/kotlin/com.chrynan.time/TimeProvider.kt
+++ time-core/src/commonMain/kotlin/com.chrynan.time/TimeProvider.kt
@@ -4,6 +4,10 @@ import kotlinx.datetime.Clock
 import kotlin.time.ExperimentalTime
 import kotlin.time.TimeSource
 
+/**
+ * An extension interface of the [TimeSource] and [Clock] interfaces. This merges those interfaces into one
+ * and provides the ability for additional functions to be added.
+ */
 @ExperimentalTime
 interface TimeProvider : TimeSource,
     Clock {
@@ -11,6 +15,11 @@ interface TimeProvider : TimeSource,
     companion object
 }
 
+/**
+ * An internal implementation of a [TimeProvider].
+ *
+ * Use the [TimeProvider] function to obtain an instance of this class.
+ */
 @ExperimentalTime
 internal class TimeProviderSource(
     timeSource: TimeSource = TimeSource.Monotonic,
@@ -19,6 +28,10 @@ internal class TimeProviderSource(
     TimeSource by timeSource,
     Clock by clock
 
+/**
+ * Creates a [TimeProvider] by delegating the [TimeSource] and [Clock] super interfaces to the provided
+ * [timeSource] and [clock].
+ */
 @ExperimentalTime
 fun TimeProvider(
     timeSource: TimeSource = TimeSource.Monotonic,
