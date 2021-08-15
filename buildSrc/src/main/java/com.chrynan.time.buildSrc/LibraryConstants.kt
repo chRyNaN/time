96becc12074e1b95d89b0f873922a37f18338e37
Chris <ckeenan@starry.com>
2021-08-15 10:29:21 -0500

    Bumped version to 0.5.1 (7)

--- buildSrc/src/main/java/com.chrynan.time.buildSrc/LibraryConstants.kt
+++ buildSrc/src/main/java/com.chrynan.time.buildSrc/LibraryConstants.kt
@@ -6,9 +6,9 @@ object LibraryConstants {
     const val group = "com.chrynan.time"
     const val owner = "chrynan"
     const val repoName = "time"
-    const val versionName = "0.5.0"
-    const val versionCode = 6
-    const val versionDescription = "Second Major Release 0.5.0"
+    const val versionName = "0.5.1"
+    const val versionCode = 7
+    const val versionDescription = "Release 0.5.1"
     const val license = "Apache-2.0"
     const val vcsUrl = "https://github.com/chRyNaN/time.git"
 
