ead0a2eb49a476b09354c78a5d621f4518de06e1
Christopher Keenan <ckeenan@starry.com>
2021-09-30 11:48:57 -0500

    Updated the TimeStamp model

--- time-core/src/commonMain/kotlin/com.chrynan.time/TimeStamp.kt
+++ time-core/src/commonMain/kotlin/com.chrynan.time/TimeStamp.kt
@@ -5,95 +5,95 @@ package com.chrynan.time
 import kotlinx.datetime.*
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.SerialName
-import kotlin.time.Duration
-import kotlin.time.ExperimentalTime
 
 /**
  * An instant in time. This can be either a UTC time [Instant] or a [LocalDateTime] for a
  * particular [TimeZone]. This sealed class encapsulates both possibilities.
  *
+ * To create an instance of this [TimeStamp] class, use either one of the [TimeStamp] or [TimeStamp] functions.
+ *
  * Events in the past are typically stored as [Instant]s which would correspond to the [Utc]
  * [TimeStamp], while events in the future (especially the far future) are typically stored as
  * [LocalDateTime] values along with their respective [TimeZone]s. This allows for properly
- * handling any volatile time changes (ex: daylight savings time).
+ * handling any volatile time changes (ex: daylight savings time). So this [TimeStamp] class is useful when you need to
+ * represent both past and future events.
+ *
+ * To perform any operations on the [TimeStamp] class, first convert it to an [Instant] using the [toInstant] function.
+ * Then it is trivial to convert it back into a [TimeStamp]. This avoids the needs of having duplicate functionality
+ * for both types and reduces the possibility of errors.
  */
 @Serializable
 sealed class TimeStamp {
 
     abstract val value: Any
+    abstract val timeZone: TimeZone?
 
+    /**
+     * A [TimeStamp] whose [value] is represented as an [Instant]. This is particularly useful for representing past or
+     * near-future time values.
+     *
+     * Note that there is no [timeZone] stored for this class, so the [timeZone] value will always be null.
+     */
     @Serializable
-    data class Utc(@SerialName(value = "value") override val value: Instant) : TimeStamp()
+    data class Utc internal constructor(@SerialName(value = "value") override val value: Instant) : TimeStamp() {
 
+        override val timeZone: TimeZone? = null
+    }
+
+    /**
+     * A [TimeStamp] whose [value] is represented as a [LocalDateTime] along with its associated [timeZone]. This is
+     * particularly useful for representing future time values. Since there can be changes in time zones, such as
+     * daylight savings time and when it is observed, it's best to store future time values as a [LocalDateTime] along
+     * with its associated [TimeZone] value. This way it can easily be converted to an [Instant] when it needs to be
+     * displayed in a UI and at that moment it will be correct. And if the observed daylight savings time changed, for
+     * instance, the [Instant] can still be properly obtained via the [LocalDateTime] and [TimeZone] when it is next
+     * loaded to be displayed in the UI.
+     *
+     * Note that there must be a [timeZone] value stored for this class, so the [timeZone] value will never be null.
+     */
     @Serializable
-    data class Local(
+    data class Local internal constructor(
         @SerialName(value = "value") override val value: LocalDateTime,
-        @SerialName(value = "timezone") val timeZone: TimeZone
+        @SerialName(value = "timezone") override val timeZone: TimeZone
     ) : TimeStamp()
+
+    companion object
 }
 
-@ExperimentalTime
-val TimeStamp.durationSinceEpoch: Duration
-    get() = toInstant().toEpochMilliseconds().milliseconds
+/**
+ * Creates an instance of a [TimeStamp] with the provided [instant].
+ *
+ * @return The [TimeStamp.Utc] value created using the provided [instant].
+ */
+@Suppress("FunctionName")
+fun TimeStamp(instant: Instant): TimeStamp.Utc = TimeStamp.Utc(value = instant)
 
-fun TimeStamp.toEpochMilliseconds(): Long = toInstant().toEpochMilliseconds()
+/**
+ * Creates an instance of a [TimeStamp] with the provided [localDateTime] and [timeZone] values.
+ *
+ * @return The [TimeStamp.Local] value created using the provided [localDateTime] and [timeZone] values.
+ */
+@Suppress("FunctionName")
+fun TimeStamp(localDateTime: LocalDateTime, timeZone: TimeZone): TimeStamp.Local =
+    TimeStamp.Local(value = localDateTime, timeZone = timeZone)
 
+/**
+ * Converts this [TimeStamp] into an [Instant]. This is useful for manipulation of the time value or displaying within
+ * a UI.
+ */
 fun TimeStamp.toInstant(): Instant =
     when (this) {
         is TimeStamp.Utc -> value
         is TimeStamp.Local -> value.toInstant(timeZone = timeZone)
     }
 
+/**
+ * Converts this [Instant] to a [TimeStamp.Utc] instance.
+ */
 fun Instant.toTimeStamp(): TimeStamp.Utc = TimeStamp.Utc(value = this)
 
+/**
+ * Converts this [LocalDateTime] to a [TimeStamp.Local] instance.
+ */
 fun LocalDateTime.toTimeStamp(timeZone: TimeZone): TimeStamp.Local =
     TimeStamp.Local(value = this, timeZone = timeZone)
-
-@ExperimentalTime
-fun TimeStamp.toUtcMillisSinceEpoch(): UtcMillisSinceEpoch =
-    when (this) {
-        is TimeStamp.Utc -> value.toUtcMillisSinceEpoch()
-        is TimeStamp.Local -> value.toUtcMillisSinceEpoch(timeZone = timeZone)
-    }
-
-@ExperimentalTime
-fun UtcMillisSinceEpoch.toTimeStamp(): TimeStamp.Utc =
-    TimeStamp.Utc(value = toInstant())
-
-operator fun TimeStamp.compareTo(other: TimeStamp): Int =
-    toInstant().compareTo(other = other.toInstant())
-
-operator fun TimeStamp.compareTo(other: Instant): Int = toInstant().compareTo(other = other)
-
-operator fun Instant.compareTo(other: TimeStamp): Int = compareTo(other = other.toInstant())
-
-@ExperimentalTime
-operator fun TimeStamp.compareTo(other: UtcMillisSinceEpoch): Int =
-    toInstant().compareTo(other = other.toInstant())
-
-@ExperimentalTime
-operator fun UtcMillisSinceEpoch.compareTo(other: TimeStamp): Int =
-    toInstant().compareTo(other = other.toInstant())
-
-@ExperimentalTime
-operator fun TimeStamp.plus(other: Duration): TimeStamp =
-    (toUtcMillisSinceEpoch() + other.toUtcMillisSinceEpoch()).toTimeStamp()
-
-@ExperimentalTime
-operator fun TimeStamp.minus(other: Duration): TimeStamp =
-    (toUtcMillisSinceEpoch() - other.toUtcMillisSinceEpoch()).toTimeStamp()
-
-@ExperimentalTime
-operator fun Duration.plus(other: TimeStamp): TimeStamp =
-    (inWholeMilliseconds + other.toUtcMillisSinceEpoch()).toTimeStamp()
-
-@ExperimentalTime
-operator fun Duration.minus(other: TimeStamp): TimeStamp =
-    (inWholeMilliseconds - other.toUtcMillisSinceEpoch()).toTimeStamp()
-
-@ExperimentalTime
-fun Clock.nowTimeStamp(): TimeStamp = now().toTimeStamp()
-
-@ExperimentalTime
-infix fun TimeStamp.durationTo(other: TimeStamp): Duration =
-    other.durationSinceEpoch - durationSinceEpoch
