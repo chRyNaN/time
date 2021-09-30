15fb6938c3e07af30238ed33a3fec83a6aba42b5
Christopher Keenan <ckeenan@starry.com>
2021-09-30 11:27:18 -0500

    Made ClockConvention Serializable

--- time-core/src/commonMain/kotlin/com.chrynan.time/ClockConvention.kt
+++ time-core/src/commonMain/kotlin/com.chrynan.time/ClockConvention.kt
@@ -2,8 +2,13 @@
 
 package com.chrynan.time
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 enum class ClockConvention {
 
     TWENTY_FOUR_HOUR,
-    TWELVE_HOUR
+    TWELVE_HOUR;
+
+    companion object
 }
