5f7daa4d4e101825c730891d9e87f4a2a745f9b0
Christopher Keenan <ckeenan@starry.com>
2021-11-29 13:46:31 -0600

    Added documentation to TimeZoneUtils functions

--- time-core/src/commonMain/kotlin/com.chrynan.time/TimeZoneUtils.kt
+++ time-core/src/commonMain/kotlin/com.chrynan.time/TimeZoneUtils.kt
@@ -4,11 +4,24 @@ package com.chrynan.time
 
 import kotlinx.datetime.*
 
+/**
+ * Retrieves all the available [TimeZone]s that are known.
+ *
+ * This is a convenience for calling [TimeZone.availableZoneIds] and mapping the results each to a [TimeZone].
+ *
+ * @see [TimeZone.availableZoneIds]
+ */
 val TimeZone.Companion.availableTimeZones: Set<TimeZone>
     get() = availableZoneIds.map { of(it) }.toSet()
 
+/**
+ * Retrieves all the [FixedOffsetTimeZone]s at the provided [instant].
+ */
 fun TimeZone.Companion.availableFixedOffsetTimeZonesAt(instant: Instant): Set<FixedOffsetTimeZone> =
     availableTimeZones.map { it.offsetAt(instant).asTimeZone() }.toSet()
 
+/**
+ * Retrieves all the [FixedOffsetTimeZone]s at the provided [instant].
+ */
 fun FixedOffsetTimeZone.Companion.availableFixedOffsetTimeZonesAt(instant: Instant): Set<FixedOffsetTimeZone> =
     TimeZone.availableFixedOffsetTimeZonesAt(instant = instant)
