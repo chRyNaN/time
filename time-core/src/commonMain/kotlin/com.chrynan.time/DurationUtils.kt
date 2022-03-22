efc986b187dbee7e0af4b2ff252e4d19e1be044c
Christopher Keenan <ckeenan@starry.com>
2022-03-22 11:36:38 -0500

    Updated DurationUtils

--- time-core/src/commonMain/kotlin/com.chrynan.time/DurationUtils.kt
+++ time-core/src/commonMain/kotlin/com.chrynan.time/DurationUtils.kt
@@ -3,143 +3,155 @@
 package com.chrynan.time
 
 import kotlin.time.Duration
-import kotlin.time.ExperimentalTime
+import kotlin.time.DurationUnit
+import kotlin.time.toDuration
 
 /**
  * The new updated Duration API is verbose and less kotlin-esque. It prefers static function calls to convert
  * Numbers into durations instead of property accessors which are more readable. This was most likely done to
  * not "pollute" the number namespace. But I find it less friendly. So, this brings back the properties and
  * just delegates to the newer function calls.
+ *
+ * FIXME: Note that it seems that the Duration API has reverted course and un-deprecated the Int.milliseconds style
+ * extension properties for Durations. This is good news, and these will slowly be replaced with that API again.
  */
 
-@ExperimentalTime
+/**
+ * Returns a [Duration] equal to this [Int] number of nanoseconds.
+ */
 val Int.nanoseconds
-    get() = Duration.nanoseconds(this)
+    get() = this.toDuration(DurationUnit.NANOSECONDS)
 
-/** Returns a [Duration] equal to this [Long] number of nanoseconds. */
-@ExperimentalTime
+/**
+ * Returns a [Duration] equal to this [Long] number of nanoseconds.
+ */
 val Long.nanoseconds
-    get() = Duration.nanoseconds(this)
+    get() = this.toDuration(DurationUnit.NANOSECONDS)
 
 /**
  * Returns a [Duration] equal to this [Double] number of nanoseconds.
  *
  * @throws IllegalArgumentException if this `Double` value is `NaN`.
  */
-@ExperimentalTime
 val Double.nanoseconds
-    get() = Duration.nanoseconds(this)
+    get() = this.toDuration(DurationUnit.NANOSECONDS)
 
-/** Returns a [Duration] equal to this [Int] number of microseconds. */
-@ExperimentalTime
+/**
+ * Returns a [Duration] equal to this [Int] number of microseconds.
+ */
 val Int.microseconds
-    get() = Duration.microseconds(this)
+    get() = this.toDuration(DurationUnit.MICROSECONDS)
 
-/** Returns a [Duration] equal to this [Long] number of microseconds. */
-@ExperimentalTime
+/**
+ * Returns a [Duration] equal to this [Long] number of microseconds.
+ */
 val Long.microseconds
-    get() = Duration.microseconds(this)
+    get() = this.toDuration(DurationUnit.MICROSECONDS)
 
 /**
  * Returns a [Duration] equal to this [Double] number of microseconds.
  *
  * @throws IllegalArgumentException if this `Double` value is `NaN`.
  */
-@ExperimentalTime
 val Double.microseconds
-    get() = Duration.microseconds(this)
+    get() = this.toDuration(DurationUnit.MICROSECONDS)
 
-/** Returns a [Duration] equal to this [Int] number of milliseconds. */
-@ExperimentalTime
+/**
+ * Returns a [Duration] equal to this [Int] number of milliseconds.
+ */
 val Int.milliseconds
-    get() = Duration.milliseconds(this)
+    get() = this.toDuration(DurationUnit.MILLISECONDS)
 
-/** Returns a [Duration] equal to this [Long] number of milliseconds. */
-@ExperimentalTime
+/**
+ * Returns a [Duration] equal to this [Long] number of milliseconds.
+ */
 val Long.milliseconds
-    get() = Duration.milliseconds(this)
+    get() = this.toDuration(DurationUnit.MILLISECONDS)
 
 /**
  * Returns a [Duration] equal to this [Double] number of milliseconds.
  *
  * @throws IllegalArgumentException if this `Double` value is `NaN`.
  */
-@ExperimentalTime
 val Double.milliseconds
-    get() = Duration.milliseconds(this)
+    get() = this.toDuration(DurationUnit.MILLISECONDS)
 
-/** Returns a [Duration] equal to this [Int] number of seconds. */
-@ExperimentalTime
+/**
+ * Returns a [Duration] equal to this [Int] number of seconds.
+ */
 val Int.seconds
-    get() = Duration.seconds(this)
+    get() = this.toDuration(DurationUnit.SECONDS)
 
-/** Returns a [Duration] equal to this [Long] number of seconds. */
-@ExperimentalTime
+/**
+ * Returns a [Duration] equal to this [Long] number of seconds.
+ */
 val Long.seconds
-    get() = Duration.seconds(this)
+    get() = this.toDuration(DurationUnit.SECONDS)
 
 /**
  * Returns a [Duration] equal to this [Double] number of seconds.
  *
  * @throws IllegalArgumentException if this `Double` value is `NaN`.
  */
-@ExperimentalTime
 val Double.seconds
-    get() = Duration.seconds(this)
+    get() = this.toDuration(DurationUnit.SECONDS)
 
-/** Returns a [Duration] equal to this [Int] number of minutes. */
-@ExperimentalTime
+/**
+ * Returns a [Duration] equal to this [Int] number of minutes.
+ */
 val Int.minutes
-    get() = Duration.minutes(this)
+    get() = this.toDuration(DurationUnit.MINUTES)
 
-/** Returns a [Duration] equal to this [Long] number of minutes. */
-@ExperimentalTime
+/**
+ * Returns a [Duration] equal to this [Long] number of minutes.
+ */
 val Long.minutes
-    get() = Duration.minutes(this)
+    get() = this.toDuration(DurationUnit.MINUTES)
 
 /**
  * Returns a [Duration] equal to this [Double] number of minutes.
  *
  * @throws IllegalArgumentException if this `Double` value is `NaN`.
  */
-@ExperimentalTime
 val Double.minutes
-    get() = Duration.minutes(this)
+    get() = this.toDuration(DurationUnit.MINUTES)
 
-/** Returns a [Duration] equal to this [Int] number of hours. */
-@ExperimentalTime
+/**
+ * Returns a [Duration] equal to this [Int] number of hours.
+ */
 val Int.hours
-    get() = Duration.hours(this)
+    get() = this.toDuration(DurationUnit.HOURS)
 
-/** Returns a [Duration] equal to this [Long] number of hours. */
-@ExperimentalTime
+/**
+ * Returns a [Duration] equal to this [Long] number of hours.
+ */
 val Long.hours
-    get() = Duration.hours(this)
+    get() = this.toDuration(DurationUnit.HOURS)
 
 /**
  * Returns a [Duration] equal to this [Double] number of hours.
  *
  * @throws IllegalArgumentException if this `Double` value is `NaN`.
  */
-@ExperimentalTime
 val Double.hours
-    get() = Duration.hours(this)
+    get() = this.toDuration(DurationUnit.HOURS)
 
-/** Returns a [Duration] equal to this [Int] number of days. */
-@ExperimentalTime
+/**
+ * Returns a [Duration] equal to this [Int] number of days.
+ */
 val Int.days
-    get() = Duration.days(this)
+    get() = this.toDuration(DurationUnit.DAYS)
 
-/** Returns a [Duration] equal to this [Long] number of days. */
-@ExperimentalTime
+/**
+ * Returns a [Duration] equal to this [Long] number of days.
+ */
 val Long.days
-    get() = Duration.days(this)
+    get() = this.toDuration(DurationUnit.DAYS)
 
 /**
  * Returns a [Duration] equal to this [Double] number of days.
  *
  * @throws IllegalArgumentException if this `Double` value is `NaN`.
  */
-@ExperimentalTime
 val Double.days
-    get() = Duration.days(this)
+    get() = this.toDuration(DurationUnit.DAYS)
