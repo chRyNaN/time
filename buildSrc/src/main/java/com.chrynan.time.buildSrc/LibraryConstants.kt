4f24a237c1505743d212c28ce66511f6a187cbaa
Chris <chrynan@pm.com>
2022-10-07 20:53:18 -0500

    Bumped version to 0.8.1 (19)

--- buildSrc/src/main/java/com.chrynan.time.buildSrc/LibraryConstants.kt
+++ buildSrc/src/main/java/com.chrynan.time.buildSrc/LibraryConstants.kt
@@ -6,8 +6,8 @@ object LibraryConstants {
     const val group = "com.chrynan.time"
     const val owner = "chrynan"
     const val repoName = "time"
-    const val versionName = "0.8.0"
-    const val versionCode = 18
+    const val versionName = "0.8.1"
+    const val versionCode = 19
     const val versionDescription = "Release $versionName ($versionCode)"
     const val license = "Apache-2.0"
     const val vcsUrl = "https://github.com/chRyNaN/time.git"
